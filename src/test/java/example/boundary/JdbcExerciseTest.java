19f73e48f64e2aa53f2b7d860b8f07a6e3f78ebc
Krzysztof Ambroziak <krzysztof.ambroziak2@bm.pl>
2021-04-08 19:32:02 +0200

    20210408 1930

--- src/test/java/example/boundary/JdbcExerciseTest.java
+++ src/test/java/example/boundary/JdbcExerciseTest.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.sql.*;
+import java.util.Arrays;
+import java.util.function.Function;
 
 public class JdbcExerciseTest {
 
@@ -53,23 +55,23 @@ public class JdbcExerciseTest {
              Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
              ResultSet resultSet = statement.executeQuery("select * from example_users")) {
 
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 String name = resultSet.getString("name");
                 String name2 = resultSet.getString(2);
                 int userId = resultSet.getInt("id");
-                System.out.println("User name is: "+name);
-                System.out.println("User name by index is: "+name2);
-                System.out.println("User id is: "+userId);
+                System.out.println("User name is: " + name);
+                System.out.println("User name by index is: " + name2);
+                System.out.println("User id is: " + userId);
             }
 
             resultSet.beforeFirst();
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 String name = resultSet.getString("name");
                 String name2 = resultSet.getString(2);
                 int userId = resultSet.getInt("id");
-                System.out.println("User name is: "+name);
-                System.out.println("User name by index is: "+name2);
-                System.out.println("User id is: "+userId);
+                System.out.println("User name is: " + name);
+                System.out.println("User name by index is: " + name2);
+                System.out.println("User id is: " + userId);
             }
 
         }
@@ -81,15 +83,15 @@ public class JdbcExerciseTest {
              Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
              ResultSet resultSet = statement.executeQuery("select id, name, age, password from example_users")) {
 
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 int id = resultSet.getInt(1);
                 String name = resultSet.getString(2);
                 int age = resultSet.getInt(3);
                 String password = resultSet.getString(4);
-                System.out.println("User id is: "+id);
-                System.out.println("User name is: "+name);
-                System.out.println("User age is: "+age);
-                System.out.println("User password is: "+password);
+                System.out.println("User id is: " + id);
+                System.out.println("User name is: " + name);
+                System.out.println("User age is: " + age);
+                System.out.println("User password is: " + password);
             }
 
         }
@@ -111,7 +113,7 @@ public class JdbcExerciseTest {
             resultSet.moveToInsertRow();
             resultSet.updateInt("id", 10);
             resultSet.updateString("name", "testowy");
-            resultSet.updateString("password","admin123");
+            resultSet.updateString("password", "admin123");
             resultSet.updateInt("age", 18);
             resultSet.insertRow();
         }
@@ -130,7 +132,7 @@ public class JdbcExerciseTest {
              Statement statement = connection.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_UPDATABLE)) {
 
             int deleted = statement.executeUpdate("delete from example_users");
-            System.out.println("Delete count: "+deleted);
+            System.out.println("Delete count: " + deleted);
 
             statement.execute("select * from example_users");
         }
@@ -148,6 +150,10 @@ public class JdbcExerciseTest {
         try (Connection connection = DatabaseConnectionProvider.getConnection();
              Statement statement = connection.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_UPDATABLE)) {
 
+            int count = statement.executeUpdate("update example_users set age=50 WHERE id=1");
+
+            System.out.println("Updated records: " + count);
+
         }
     }
 
@@ -161,8 +167,22 @@ public class JdbcExerciseTest {
     @Test
     public void testGetUserByNameAndPassword() throws IOException, SQLException {
         String userName = "Jan";
-        String password = "'123' OR 1=1";
+        String password = "'123456' OR 1=1";
         String sql = "select * from example_users WHERE name='" + userName + "' AND password=" + password;
+
+        if (password.contains(" ")) {
+            System.out.println("Błedne hasło");
+            return;
+        }
+
+        try (Connection connection = DatabaseConnectionProvider.getConnection();
+             Statement statement = connection.createStatement()
+        ) {
+            ResultSet resultSet = statement.executeQuery(sql);
+            if (resultSet.next()) {
+                System.out.println("Zalogowano do aplikacji!");
+            }
+        }
 
     }
 
@@ -175,8 +195,24 @@ public class JdbcExerciseTest {
     @Test
     public void testGetUserByNameAndPasswordWithPreparedStatement() throws IOException, SQLException {
         String userName = "Jan";
-        String password = "123";
+        String password = "'123456' OR 1=1";
         String sql = "select * from example_users WHERE name=? AND password=?";
+
+        try (Connection connection = DatabaseConnectionProvider.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)
+        ) {
+            statement.setString(1, userName);
+            statement.setString(2, password);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                System.out.println("Zalogowano!");
+            } else {
+                System.out.println("Błędny login lub hasło!");
+            }
+
+
+        }
 
     }
 
@@ -187,15 +223,106 @@ public class JdbcExerciseTest {
      * @throws SQLException
      */
     @Test
-    public void testInsert() throws IOException, SQLException {
-        try (Connection connection = DatabaseConnectionProvider.getConnection()) {
+    public void testInsertBatch() throws IOException, SQLException {
+        try (Connection connection = DatabaseConnectionProvider.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("insert into example_users (name,password,age)" +
+                     "values (?,?,?)")) {
+            ;
 
-            PreparedStatement preparedStatement = connection.prepareStatement("insert into example_users (name,password,age)" +
-                    "values (?,?,?)");
+            preparedStatement.setString(1, "Krzysztof");
+            preparedStatement.setString(2, "admin123");
+            preparedStatement.setInt(3, 18);
 
-        //            System.out.println("updated count: " + ints[0]);
+            preparedStatement.addBatch();
+
+            preparedStatement.setString(1, "Adam");
+            preparedStatement.setString(2, "admin1234");
+            preparedStatement.setInt(3, 45);
+
+            preparedStatement.addBatch();
+
+            int[] ints = preparedStatement.executeBatch();
+            int insertedCount = 0;
+            for (int i = 0; i < ints.length; i++) {
+                insertedCount += ints[i];
+            }
+
+            System.out.println("Inserted count: " + insertedCount);
+
+
+        }
+
+    }
+
+    @Test
+    public void testInsertBatch2() throws IOException, SQLException {
+        try (Connection connection = DatabaseConnectionProvider.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("insert into example_users (name,password,age)" +
+                     "values (?,?,?), (?,?,?)")) {
+
+            preparedStatement.setString(1, "Krzysztof");
+            preparedStatement.setString(2, "admin123");
+            preparedStatement.setInt(3, 18);
+
+            preparedStatement.setString(4, "Adam");
+            preparedStatement.setString(5, "admin1234");
+            preparedStatement.setInt(6, 45);
+
+            preparedStatement.addBatch();
+
+            int[] ints = preparedStatement.executeBatch();
+
+            int insertedCount = Arrays.stream(ints).sum();
+
+            System.out.println("Inserted count: " + insertedCount);
+
         }
     }
+
+    @Test
+    public void testBatchUpdate() throws IOException, SQLException {
+        try (Connection connection = DatabaseConnectionProvider.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("update example_users set age = ? WHERE id = ? ")) {
+
+            preparedStatement.setInt(1, 20);
+            preparedStatement.setInt(2, 1);
+
+            preparedStatement.addBatch();
+
+            preparedStatement.setInt(1, 50);
+            preparedStatement.setInt(2, 100);
+
+            preparedStatement.addBatch();
+
+            int[] ints = preparedStatement.executeBatch();
+
+            System.out.println("Update count 1: " + ints[0]);
+            System.out.println("Update count 2: " + ints[1]);
+
+            int updatedCount = Arrays.stream(ints).sum();
+
+            System.out.println("Updated count: " + updatedCount);
+        }
+
+    }
+
+    @Test
+    public void testInsert() throws IOException, SQLException {
+        try (Connection connection = DatabaseConnectionProvider.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("insert into example_users (name,password,age)" +
+                     "values (?,?,?)")) {
+
+            preparedStatement.setString(1, "Andrzej");
+            preparedStatement.setString(2, "111");
+            preparedStatement.setInt(3, 30);
+
+            int insertedCount = preparedStatement.executeUpdate();
+
+            System.out.println("Inserted count: " + insertedCount);
+
+        }
+    }
+
 
     /**
      * Test demonstruje wykonanie dwóch operacji w jednej transakcji
@@ -205,9 +332,37 @@ public class JdbcExerciseTest {
      */
     @Test
     public void testInsertWithTransaction() throws IOException, SQLException {
-        Connection connection = DatabaseConnectionProvider.getConnection();
+        try (Connection connection = DatabaseConnectionProvider.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("insert into example_users (name,password,age)" +
+                     "values (?,?,?)")) {
+
+            connection.setAutoCommit(false);
+
+            preparedStatement.setString(1, "User");
+            preparedStatement.setString(2, "test");
+            preparedStatement.setInt(3, 30);
+
+            preparedStatement.executeUpdate();
+
+            preparedStatement.setString(1, "User2");
+            preparedStatement.setString(2, "testowy2");
+            preparedStatement.setInt(3, 20);
+
+            preparedStatement.executeUpdate();
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from example_users");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("name"));
+            }
+
+            connection.commit();
+
+        }
         //try {
-          //  connection.setAutoCommit(false);
+        //  connection.setAutoCommit(false);
 
     }
 
@@ -220,10 +375,53 @@ public class JdbcExerciseTest {
      */
     @Test
     public void testInsertWithTransaction2() throws IOException, SQLException {
-    //    Connection connection = DatabaseConnectionProvider.getConnection();
-//        try {
-  //          connection.setAutoCommit(false);
+        try (Connection connection = DatabaseConnectionProvider.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("insert into example_users (name,password,age)" +
+                     "values (?,?,?)")) {
 
+            connection.setAutoCommit(false);
+
+            preparedStatement.setString(1, "User");
+            preparedStatement.setString(2, "test");
+            preparedStatement.setInt(3, 30);
+
+            preparedStatement.executeUpdate();
+
+            preparedStatement.setString(1, "User2");
+            preparedStatement.setString(2, "testowy2");
+            preparedStatement.setInt(3, 20);
+
+            preparedStatement.executeUpdate();
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from example_users");
+
+            while (resultSet.next()) {
+                System.out.println("In transaction: " + resultSet.getString("name"));
+            }
+
+            try (Connection newConnection = DatabaseConnectionProvider.getConnection();
+                 PreparedStatement statementFromNewTransaction = newConnection.prepareStatement("select * from example_users WHERE name in (?,?)")
+            ) {
+                statementFromNewTransaction.setString(1, "User");
+                statementFromNewTransaction.setString(2, "User2");
+
+                ResultSet resultSetFromNewTransaction = statementFromNewTransaction.executeQuery();
+
+                if (resultSetFromNewTransaction.next()) {
+                    System.out.println("User found");
+                } else {
+                    System.out.println("No user found");
+                }
+
+
+            }
+
+
+            connection.commit();
+
+        }
 
     }
 }
